c77d699e2e77bd325ef984c2fe2e5a6fde1a727c
yuu <crosscore.g@gmail.com>
2024-01-30 14:40:09 +0900

    update MovingCircle.js

--- src/components/MovingCircle.js
+++ src/components/MovingCircle.js
@@ -2,15 +2,17 @@
 import React, { useEffect, useState } from "react";
 
 const MovingCircle = ({ startMoving }) => {
-  const [position, setPosition] = useState(-100);
-  const [diameter, setDiameter] = useState(0); // diameterの初期値を0に設定
-  const speed = 60; // px per second
+  const [position, setPosition] = useState(0); // 初期値を0に設定
+  const [diameter, setDiameter] = useState(0);
+  const speed = 120; // px per second
   const animationFrameRate = 60; // 60 frames per second
 
   useEffect(() => {
     // ウィンドウサイズに基づいてdiameterを設定
-    const newDiameter = typeof window !== "undefined" ? window.innerWidth / 5 : 0;
+    const newDiameter =
+      typeof window !== "undefined" ? window.innerWidth / 5 : 0;
     setDiameter(newDiameter);
+    setPosition(-newDiameter);
 
     const moveCircle = () => {
       setPosition((prevPosition) => {
@@ -30,18 +32,20 @@ const MovingCircle = ({ startMoving }) => {
   }, [startMoving]);
 
   // diameterが設定された後でスタイルを定義
-  const circleStyle = diameter > 0 && typeof window !== "undefined" ? {
-    width: diameter,
-    height: diameter,
-    borderRadius: "50%",
-    backgroundColor: "blue",
-    position: "absolute",
-    left: position,
-    top: window.innerHeight / 2 - diameter / 2, // 中央に配置
-  } : null;
+  const circleStyle =
+    diameter > 0 && typeof window !== "undefined"
+      ? {
+          width: diameter,
+          height: diameter,
+          borderRadius: "50%",
+          backgroundColor: "blue",
+          position: "absolute",
+          left: position,
+          top: window.innerHeight / 2 - diameter / 2, // 中央に配置
+        }
+      : null;
 
   return diameter > 0 ? <div style={circleStyle} /> : null;
 };
 
 export default MovingCircle;
-
