ac5d72a0ca14a3f359cabda36906cfe1950df57e
yuu <crosscore.g@gmail.com>
2024-02-01 15:59:44 +0900

    update websocket-server.js

--- src/pages/websocket-server.js
+++ src/pages/websocket-server.js
@@ -4,8 +4,15 @@ const wss = new WebSocket.Server({ port: 8081 });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(data) {
-    const message = data.toString();
-    console.log("message from client:", message);
+    // Convert the received data to a string and parse it as JSON
+    const message = JSON.parse(data.toString());
+
+    // Log the message with the client identifier
+    if (message.client) {
+      console.log(`message from ${message.client}:`, message);
+    } else {
+      console.log("message from unknown client:", message);
+    }
   });
 });
 
